8cb6b803e1b76a112c23ca35f511a2baf560a10e
Sidong <sidongh@gmail.com>
2014-04-10 14:31:36 +0800

    fix admin

--- routes/site.js
+++ routes/site.js
@@ -41,8 +41,8 @@ exports.report = function (req, res) {
 		req.flash('error', '请登录！');
 		return res.redirect('/signin');
 	}
-	// if (!config.admins.hasOwnProperty(req.session.user.name)) {
-	if (!req.session.user.is_admin) {
+	if (!config.admins.hasOwnProperty(req.session.user.name)) {
+	// if (!req.session.user.is_admin) {
 		return res.redirect('/404');
 	}
 	User.find({}, function (err, users) {
