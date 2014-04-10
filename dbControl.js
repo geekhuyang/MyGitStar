0de20a0d3cab24db81a110fb8e988cea6663b692
Sidong <sidongh@gmail.com>
2014-04-10 15:30:23 +0800

    test

--- dbControl.js
+++ dbControl.js
@@ -1,13 +1,31 @@
-// var EventProxy = require('eventproxy');
-var fs = require('fs');
-var mongoose = require('mongoose');
-var myGit = require('./lib/myGit');
 var _ = require('underscore');
-var url = require('url');
+var mongoose = require('mongoose');
 
-var User = require('./proxy').User;
+// var host = 'mongodb://stone:6612683@oceanic.mongohq.com:10010/mygitstar';
 
-var config = require('./config').config;
-var host = url.parse(config.host);
-console.log(host);
+// var db = mongoose.connect(host, function (err) {
+// 	"use strict";
+// 	if (err) {
+// 		console.error('connect to %s error: ', host, err.message);
+// 		// process.exit(1);
+// 	}
+// });
 
+// var User = require('./models').User;
+// var StarItem = require('./models').StarItem;
+
+// User.find({}, function (err, users) {
+// 	if (err) {
+// 		return console.log(err);
+// 	}
+// 	users = _.sortBy(users, function (user) { return -user.visit; });
+// 	StarItem.count({}, function (err, count) {
+// 		if (err) {
+// 			return console.log(err);
+// 		}
+// 		console.log('count: ', count);
+// 			// users: users,
+// 			// format_date: format_date,
+// 			// count: count
+// 	});
+// });
